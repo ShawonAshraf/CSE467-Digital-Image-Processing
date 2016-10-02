ef4c71dbb19e4f19e6765713e231089f72346078
Shawon Ashraf <shawon13@live.com>
2016-10-03 00:16:10 +0600

    Done assignment2

---
+++ assignment2.m
@@ -0,0 +1,41 @@
+image = imread('rice.png');
+%show image using otsu's method
+t_otsu = graythresh(image);
+figure
+bw_otsu = im2bw(image, t_otsu);
+imshow(bw_otsu)
+
+
+%now minimized algorithm
+
+n = 256;
+c = imhist(image, n);
+sum_c = sum(c);
+p = c / sum_c; % probability
+
+for i = 1 : n
+    low = sum(p(1:i));
+    high = sum(p(i+1 : n));
+    mean_low = sum( (p(1 : i) .* (1 : i)') ) / low;
+    mean_high = sum( (p(i+1 : end) .* (i+1 : n)') ) / high ;
+    
+    % get variance
+    
+    ii1 = (1:i)';
+    ii2 = (i+1:n)';
+    
+    t1 = sum((ii1 - mean_low).^2 .* p(1:i));
+    t2 = sum(((ii2 - mean_high).^2 .* p(i+1:n)));
+    
+    v1 = t1 / low;
+    v2 = t2 / high;
+    
+    
+    %v(i) = low * high * ((mean_low - mean_high)^2);
+    v(i) = (low * v1) + (high * v2);
+end
+
+t_min = min(v(:));
+bw_min = im2bw(image, t_min);
+figure
+imshow(bw_min)
